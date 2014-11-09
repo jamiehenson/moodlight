a8cc6c20e7f491d18b6492fc609927b6d3bc304f
Jamie Henson <jhenson47@gmail.com>
2014-11-08 20:45:45 -0800

    Better time algo and simplified css inputs

--- ambiviz.js
+++ ambiviz.js
@@ -1,15 +1,21 @@
-function ambiviz(selector, location, snip1, snip2){
-  get_weather(selector, location, snip1, snip2);
+function Ambiviz(options)
+{
+  // this.options = options;
+  // this.selector = options.selector;
+  // this.location = options.location;
+  // this.css = options.css;
+  // this.
+  get_data(options.selector, options.location, options.css);
 }
 
-function get_weather(sel, loc, snip1, snip2)
+function get_data(sel, loc, css)
 {
   request = new XMLHttpRequest();
   request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + loc + '&units=metric', true);
 
   request.onload = function() {
     if (request.status >= 200 && request.status < 400){
-      process_weather(sel, JSON.parse(request.responseText), snip1, snip2);
+      process_weather(sel, JSON.parse(request.responseText), css);
     } else {
       console.log('We reached our target server, but it returned an error');
     }
@@ -25,66 +31,91 @@ function get_weather(sel, loc, snip1, snip2)
 
 function process_time(weather)
 {
-  var start = new Date().setHours(0,0,0,0);
-  var end = new Date().setHours(23,59,59,999);
-  var now = Math.floor(new Date().getTime() / 1000);
-  var mid = parseInt((weather.sys.sunset + weather.sys.sunrise) / 2);
-  var progress = 1-(Math.abs(mid-now) / (weather.sys.sunset - weather.sys.sunrise));
+  var start = Math.floor(new Date().setHours(0,0,0,0) / 1000);
+  var now = ((Math.floor(new Date().getTime() / 1000))) - weather.sys.sunrise;
 
-  var p_lerp = parseInt(progress * 255);
-  var col = hex_assemble(p_lerp,p_lerp,p_lerp);
-  var anti_col = hex_assemble(255-p_lerp,255-p_lerp,255-p_lerp);
+  if ((weather.sys.sunrise - start) > 86400)
+  {
+    now += 86400;
+  }
+  else if ((weather.sys.sunrise - start) < -86400)
+  {
+    now -= 86400;
+  }
 
-  return '({"start" : ' + start
-    + ', "end" : ' + end
-    + ', "now" : ' + now
-    + ', "progress" : ' + progress
+  var mid = parseInt(((weather.sys.sunset + weather.sys.sunrise) / 2) - weather.sys.sunrise);
+  var intensity = (Math.abs(mid-now) <= mid) ? (mid - (Math.abs(mid-now))) / mid : 0;
+
+  return '({"now" : ' + now
+    + ', "mid" : ' + mid
+    + ', "intensity" : ' + intensity
     + '})'
 }
 
-function process_weather(sel, weather, snip1, snip2)
+function process_weather(element, weather, css)
 {
-  time = eval(process_time(weather));
+  var time = eval(process_time(weather));
 
-  $(sel + "-footer").html(weather.main.temp + "&deg;C - " + weather.name + ", " + weather.sys.country);
-  if (time.now < weather.sys.sunset && time.now > weather.sys.sunrise)
+  if (document.getElementById(element + "-footer"))
   {
-    for (var key in snip1) {
-      if (snip1.hasOwnProperty(key)) {
-        var new_val = interpolate(snip1[key], snip2[key], time.progress);
-        console.log(new_val);
-        document.getElementById(sel.slice(1)).style[key] = new_val;
-      }
+    document.getElementById(element + "-footer").innerHTML = weather.main.temp + "&deg;C - " + weather.name + ", " + weather.sys.country;
+  }
+
+  var existing = eval(grab_existing(element, css));
+
+  for (var key in css)
+  {
+    if (css.hasOwnProperty(key)) 
+    {
+      var new_val = interpolate(existing[key], css[key], time.intensity);
+      console.log(existing, css, new_val)
+      document.getElementById(element).style[key] = new_val + " !important";
     }
-
-    bg = hex_assemble(255*time.progress, 255*time.progress, 255*time.progress);
-    col = hex_assemble(255-(255*time.progress), 255-(255*time.progress), 255-(255*time.progress));
   }
-  else
+}
+
+function grab_existing(element, css)
+{
+  var el = document.getElementById(element);
+  var style = window.getComputedStyle(el);
+  var styles = "({";
+  for (var key in css) 
   {
-    bg = "#000011";
-    col = "#FFFFFF";
+    if (css.hasOwnProperty(key)) 
+    {
+      styles += '"' + key + '" : "' + style.getPropertyValue(key) + '",';
+    }
   }
-
-  // $(sel).velocity({ 
-  //   backgroundColor: bg,
-  //   color: col 
-  // }, 3000); 
+  styles.slice(0, -1);
+  styles += "})";
+  return styles;
 }
 
 function interpolate(val1, val2, t)
 {
-  if (val1[0] == "#" && val2[0] == "#")
+  if (val1[0] == "#")
   {
-    var r = lerp( to_dec(val1[1] + val1[2]), to_dec(val2[1] + val2[2]), t );
-    var g = lerp( to_dec(val1[3] + val1[4]), to_dec(val2[3] + val2[4]), t );
-    var b = lerp( to_dec(val1[5] + val1[6]), to_dec(val2[5] + val2[6]), t );
-    return hex_assemble(r,g,b);
+    val1 = hex_disassemble(val1);
   }
-  else if (val1[0] == "rgb" && val2[0] == "rgb")
+  else if (val1[0] == "r")
   {
+    val1 = val1.slice(4).slice(0,-1).split(", ");
+  }
 
+  if (val2[0] == "#")
+  {
+    val2 = hex_disassemble(val2);
   }
+  else if (val2[0] == "r")
+  {
+    val2 = val2.slice(4).slice(0,-1).split(", ");
+  }
+
+  var r = lerp( val1[0], val2[0], t );
+  var g = lerp( val1[1], val2[1], t );
+  var b = lerp( val1[2], val2[2], t );
+
+  return "rgb(" + r + "," + g + "," + b + ")";
 }
 
 function lerp(val1, val2, t)
@@ -104,9 +135,12 @@ function hex_assemble(r,g,b)
   return "#" + to_hex(parseInt(r)) + to_hex(parseInt(g)) + to_hex(parseInt(b));
 }
 
-function hex_disassemble(r,g,b)
+function hex_disassemble(hex)
 {
-  return [to_dec(parseInt(r)), to_dec(parseInt(g)), to_dec(parseInt(b))];
+  r = hex[1] + hex[2];
+  g = hex[3] + hex[4];
+  b = hex[5] + hex[6];
+  return [to_dec(r), to_dec(g), to_dec(b)];
 }
 
 function to_hex(val) 
